fb7b53185525496394997f04f1fe0a3c25629a1d
lizhen <1553164564@qq.com>
2018-10-25 17:45:11 +0800

    小修改@Deprecated

--- src/main/java/com/lizhen/MyConnectionFactory.java
+++ src/main/java/com/lizhen/MyConnectionFactory.java
@@ -6,6 +6,7 @@ import org.apache.activemq.ActiveMQConnectionFactory;
  * Created by lizhen on 2018/9/11.
  * 定义自己的连接ActiveMQ的连接工厂，这里没有用到
  */
+@Deprecated
 public class MyConnectionFactory extends ActiveMQConnectionFactory {
     @Override
     public void setClientID(String clientID) {
